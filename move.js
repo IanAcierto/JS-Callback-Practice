ec734b99a5c7d06de9d5afa708e1b9a23040dfd0
IanAcierto <101164364+IanAcierto@users.noreply.github.com>
2022-05-18 18:07:52 -0700

    Add files via upload

--- move.js
+++ move.js
@@ -6,7 +6,57 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    function moveWithArrowKeys(){
+        function moveWithArrowKeys(left, bottom, callback) {
+            let direction = null
+            let x = left
+            let y = bottom
+        
+            function moveCharacter(){
+              if (direction === 'west'){
+                x = x-1
+              }
+              if (direction === 'east'){
+                x = x+1
+              }
+              if (direction === 'north'){
+                y = y+1
+              }
+              if (direction === 'south'){
+                y = y-1
+              }
+              element.style.left = x + 'px'
+              element.style.bottom = y + 'px'
+          }
+        setInterval(moveCharacter, 1)
+        }
+        //set keys to direction
+        document.addEventListener('keydown', function(e){
+          if (e.repeat)return;
+          if(e.key ='arrowleft'){
+            direction = 'west'
+          }
+          if (e.key ='arrowright'){
+            direction = 'east'
+          }
+          if (e.key ='arrowup'){
+            direction = 'north'
+          }
+          if (e.key ='arrowdown'){
+            direction = 'south'
+          }
+          callback(direction)
+        })
+        //no direction when no key is pressed
+        document.addEventListener('keyup', function(e){
+          direction = null
+          callback(direction)
+        })
+        
+        setInterval(moveCharacter, 1)
+    }
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
